7cb3a76a2923204e8aaa847c29fdb5029a36699d
Yann Moisan <yamo93@gmail.com>
2012-01-07 23:02:30 +0100

    Define variables for working dir

--- blog.sh
+++ blog.sh
@@ -4,19 +4,20 @@
 # Mettre un lien vers BlackBelt
 # Formatage auto de la CSS avec VIM
 # woorank 35.7 --> 41.7 (icon, lang, rss, sitemap) 
-blog_dir=src
+src_dir=src
+dst_dir=target
 
-cd $blog_dir
+#cd $blog_dir
 
-rm -r target
-mkdir target
+rm -r $dst_dir
+mkdir $dst_dir
 
-csplit template.html "/<!-- entry -->/" {*}
+csplit $src_dir/template.html "/<!-- entry -->/" {*}
 
-other_files=(`find . -name "*.html"|grep -v "entry.*html"`)
+other_files=(`find $src_dir -name "*.html"|grep -v "entry.*html"`)
 o=${#other_files[@]}
 
-entry_files=(`find . -name "e*"|sort`)
+entry_files=(`find $src_dir -name "entry*"|sort`)
 echo nb entry_files:${#entry_files[@]}
 l=${#entry_files[@]}
 echo l:$l
@@ -26,55 +27,56 @@ for ((i=0;i<o;i++));do
   echo Processing:${other_files[$i]}
   
   title=`awk 'BEGIN{ RS="</h2>"}{gsub(/.*<h2 class="entry-title">/,"")}1{print $RS;exit}' ${other_files[$i]}`
-  echo ${other_files[$i]} >> target/menu.xml
-  cat xx00 > target/${other_files[$i]}
+  echo ${other_files[$i]} >> $dst_dir/menu.xml
+  dst_file=$dst_dir/${other_files[$i]#src/}
+  cat xx00 > $dst_file
   
-  cat ${other_files[$i]} >> target/${other_files[$i]}
-  cat xx02 >> target/${other_files[$i]}
+  cat ${other_files[$i]} >> $dst_file
+  cat xx02 >> $dst_file
 
-  sed -i "s/<!-- title -->/$title/" target/${other_files[$i]}
+  sed -i "s/<!-- title -->/$title/" $dst_file
 done
 
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>" > target/rss.xml
-echo "<rss version=\"2.0\">" >> target/rss.xml
-echo "<channel>" >> target/rss.xml 
-echo "<title>Yann Moisan</title>" >> target/rss.xml
-echo "<description>Yann Moisan RSS feed</description>" >> target/rss.xml
-echo "<link>http://www.yannmoisan.com</link>" >> target/rss.xml
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>" > $dst_dir/rss.xml
+echo "<rss version=\"2.0\">" >> $dst_dir/rss.xml
+echo "<channel>" >> $dst_dir/rss.xml 
+echo "<title>Yann Moisan</title>" >> $dst_dir/rss.xml
+echo "<description>Yann Moisan RSS feed</description>" >> $dst_dir/rss.xml
+echo "<link>http://www.yannmoisan.com</link>" >> $dst_dir/rss.xml
 
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > target/sitemap.xml
-echo "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">" >> target/sitemap.xml
-echo "<url><loc>http://www.yannmoisan.com/index.html</loc></url>"  >> target/sitemap.xml
-echo "<url><loc>http://www.yannmoisan.com/a-propos.html</loc></url>"  >> target/sitemap.xml
-echo "<url><loc>http://www.yannmoisan.com/cv.html</loc></url>"  >> target/sitemap.xml
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > $dst_dir/sitemap.xml
+echo "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">" >> $dst_dir/sitemap.xml
+echo "<url><loc>http://www.yannmoisan.com/index.html</loc></url>"  >> $dst_dir/sitemap.xml
+echo "<url><loc>http://www.yannmoisan.com/a-propos.html</loc></url>"  >> $dst_dir/sitemap.xml
+echo "<url><loc>http://www.yannmoisan.com/cv.html</loc></url>"  >> $dst_dir/sitemap.xml
 
 for ((i=0;i<l;i++));do
   in=${entry_files[$i]}
   # Pourquoi ./ avec la commande find
-  fout=${in#./entry*-}
-  out=target/$fout 
+  fout=${in#src/entry*-}
+  out=$dst_dir/$fout 
   
   title=`awk 'BEGIN{ RS="</h2>"}{gsub(/.*<h2 class="entry-title">/,"")}1{print $RS;exit}' $in`
   echo Processing entry $((i+1))/$l : in=$in out=$out title=$title
 
-  echo ${entry_files[$i]} >> target/menu.xml
+  echo ${entry_files[$i]} >> $dst_dir/menu.xml
   cat xx00 > $out
  
-  echo "<item>"  >> target/rss.xml
-   echo "<title>$title</title>"  >> target/rss.xml
-   echo "<description>$title</description>"  >> target/rss.xml
-   echo "<link>http://www.yannmoisan.com/$fout</link>"  >> target/rss.xml
-   echo "<guid>http://www.yannmoisan.com/$fout</guid>"  >> target/rss.xml
-   echo "</item>"  >> target/rss.xml
+  echo "<item>"  >> $dst_dir/rss.xml
+   echo "<title>$title</title>"  >> $dst_dir/rss.xml
+   echo "<description>$title</description>"  >> $dst_dir/rss.xml
+   echo "<link>http://www.yannmoisan.com/$fout</link>"  >> $dst_dir/rss.xml
+   echo "<guid>http://www.yannmoisan.com/$fout</guid>"  >> $dst_dir/rss.xml
+   echo "</item>"  >> $dst_dir/rss.xml
 
-echo "<url><loc>http://www.yannmoisan.com/$fout</loc></url>"  >> target/sitemap.xml
+echo "<url><loc>http://www.yannmoisan.com/$fout</loc></url>"  >> $dst_dir/sitemap.xml
 
 
   echo "<p class=\"nav\">"  >> $out
   if [ $i -eq 0 ];then
     prev="first"
   else
-    prev=${entry_files[$i-1]#./entry*-}
+    prev=${entry_files[$i-1]#src/entry*-}
     echo "<a href=\"$prev\">Billet précédent</a>" >> $out
     echo "<span>-</span>"  >> $out
   fi
@@ -82,7 +84,7 @@ echo "<url><loc>http://www.yannmoisan.com/$fout</loc></url>"  >> target/sitemap.
   if [ $i -eq $((l-1)) ];then
     next="last"
   else
-    next=${entry_files[$i+1]#./entry*-}
+    next=${entry_files[$i+1]#src/entry*-}
     echo "<a href=\"$next\">Billet suivant</a>" >> $out
   fi
 
@@ -95,12 +97,12 @@ echo "<url><loc>http://www.yannmoisan.com/$fout</loc></url>"  >> target/sitemap.
   sed -i "s/<!-- title -->/$title/" $out
 done
 
-echo "</channel>" >> target/rss.xml
-echo "</rss>" >> target/rss.xml
+echo "</channel>" >> $dst_dir/rss.xml
+echo "</rss>" >> $dst_dir/rss.xml
 
-echo "</urlset>"  >> target/sitemap.xml
+echo "</urlset>"  >> $dst_dir/sitemap.xml
 
-cp style.css target/style.css
-cp favicon.ico target/favicon.ico
-cp belt3_L.gif target/belt3_L.gif
-cp $out target/index.html
+cp $src_dir/style.css $dst_dir/style.css
+cp $src_dir/favicon.ico $dst_dir/favicon.ico
+cp $src_dir/belt3_L.gif $dst_dir/belt3_L.gif
+cp $out $dst_dir/index.html
