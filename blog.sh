fab3234d7f11f16c86c282aa20d3320abb7696f4
Yann Moisan <yamo93@gmail.com>
2012-01-08 01:55:00 +0100

    format

--- blog.sh
+++ blog.sh
@@ -24,17 +24,17 @@ echo l:$l
 
 # TODO:Creer une méthode qui prend un tableau de fichiers et un mode : isEntry
 for file in "${other_files[@]}";do
-  echo Processing:$file
-  
-  title=`awk 'BEGIN{ RS="</h2>"}{gsub(/.*<h2 class="entry-title">/,"")}1{print $RS;exit}' $file`
-  echo ${file} >> $dst_dir/menu.xml
-  dst_file=$dst_dir/${file#src/}
-  cat xx00 > $dst_file
-  
-  cat ${file} >> $dst_file
-  cat xx02 >> $dst_file
+    echo Processing:$file
 
-  sed -i "s/<!-- title -->/$title/" $dst_file
+    title=`awk 'BEGIN{ RS="</h2>"}{gsub(/.*<h2 class="entry-title">/,"")}1{print $RS;exit}' $file`
+    echo ${file} >> $dst_dir/menu.xml
+    dst_file=$dst_dir/${file#src/}
+    cat xx00 > $dst_file
+
+    cat ${file} >> $dst_file
+    cat xx02 >> $dst_file
+
+    sed -i "s/<!-- title -->/$title/" $dst_file
 done
 
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>" > $dst_dir/rss.xml
@@ -51,50 +51,50 @@ echo "<url><loc>http://www.yannmoisan.com/a-propos.html</loc></url>"  >> $dst_di
 echo "<url><loc>http://www.yannmoisan.com/cv.html</loc></url>"  >> $dst_dir/sitemap.xml
 
 for ((i=0;i<l;i++));do
-  in=${entry_files[$i]}
-  # Pourquoi ./ avec la commande find
-  fout=${in#src/entry*-}
-  out=$dst_dir/$fout 
-  
-  title=`awk 'BEGIN{ RS="</h2>"}{gsub(/.*<h2 class="entry-title">/,"")}1{print $RS;exit}' $in`
-  echo Processing entry $((i+1))/$l : in=$in out=$out title=$title
+    in=${entry_files[$i]}
+    # Pourquoi ./ avec la commande find
+    fout=${in#src/entry*-}
+    out=$dst_dir/$fout 
 
-  echo ${entry_files[$i]} >> $dst_dir/menu.xml
-  cat xx00 > $out
- 
-  echo "<item>"  >> $dst_dir/rss.xml
-   echo "<title>$title</title>"  >> $dst_dir/rss.xml
-   echo "<description>$title</description>"  >> $dst_dir/rss.xml
-   echo "<link>http://www.yannmoisan.com/$fout</link>"  >> $dst_dir/rss.xml
-   echo "<guid>http://www.yannmoisan.com/$fout</guid>"  >> $dst_dir/rss.xml
-   echo "</item>"  >> $dst_dir/rss.xml
+    title=`awk 'BEGIN{ RS="</h2>"}{gsub(/.*<h2 class="entry-title">/,"")}1{print $RS;exit}' $in`
+    echo Processing entry $((i+1))/$l : in=$in out=$out title=$title
 
-echo "<url><loc>http://www.yannmoisan.com/$fout</loc></url>"  >> $dst_dir/sitemap.xml
+    echo ${entry_files[$i]} >> $dst_dir/menu.xml
+    cat xx00 > $out
+
+    echo "<item>"  >> $dst_dir/rss.xml
+    echo "<title>$title</title>"  >> $dst_dir/rss.xml
+    echo "<description>$title</description>"  >> $dst_dir/rss.xml
+    echo "<link>http://www.yannmoisan.com/$fout</link>"  >> $dst_dir/rss.xml
+    echo "<guid>http://www.yannmoisan.com/$fout</guid>"  >> $dst_dir/rss.xml
+    echo "</item>"  >> $dst_dir/rss.xml
+
+    echo "<url><loc>http://www.yannmoisan.com/$fout</loc></url>"  >> $dst_dir/sitemap.xml
 
 
-  echo "<p class=\"nav\">"  >> $out
-  if [ $i -eq 0 ];then
-    prev="first"
-  else
-    prev=${entry_files[$i-1]#src/entry*-}
-    echo "<a href=\"$prev\">Billet précédent</a>" >> $out
-    echo "<span>-</span>"  >> $out
-  fi
-  
-  if [ $i -eq $((l-1)) ];then
-    next="last"
-  else
-    next=${entry_files[$i+1]#src/entry*-}
-    echo "<a href=\"$next\">Billet suivant</a>" >> $out
-  fi
+    echo "<p class=\"nav\">"  >> $out
+    if [ $i -eq 0 ];then
+        prev="first"
+    else
+        prev=${entry_files[$i-1]#src/entry*-}
+        echo "<a href=\"$prev\">Billet précédent</a>" >> $out
+        echo "<span>-</span>"  >> $out
+    fi
 
-  
-  echo "</p>"  >> $out
+    if [ $i -eq $((l-1)) ];then
+        next="last"
+    else
+        next=${entry_files[$i+1]#src/entry*-}
+        echo "<a href=\"$next\">Billet suivant</a>" >> $out
+    fi
 
-  cat ${entry_files[$i]} >> $out
-  cat xx02 >> $out 
-  
-  sed -i "s/<!-- title -->/$title/" $out
+
+    echo "</p>"  >> $out
+
+    cat ${entry_files[$i]} >> $out
+    cat xx02 >> $out 
+
+    sed -i "s/<!-- title -->/$title/" $out
 done
 
 echo "</channel>" >> $dst_dir/rss.xml
